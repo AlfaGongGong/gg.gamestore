ab407012451387ff722c8f600e8351e6ba8f3788
Jasenko Bozinovic <jasenkobozinovic@gmail.com>
2024-06-01 01:42:53 +0200

    feat: Add routing to the application for different pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import HomePage from './pages/HomePage.js';
+import AdminDashboard from './pages/AdminDashboard.js';
+import UserDashboard from './pages/UserDashboard.js';
+import NotFoundPage from './pages/NotFoundPage.js';
+
 function App() {
   return (
-    <div className="App">
-        <h1>
-          Welcome to React
-        </h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/user-dashboard" component={UserDashboard} />
+        <Route path="/admin-dashboard" component={AdminDashboard} />
+        <Route path="/404" component={NotFoundPage} />
+        <Redirect to="/404" />
+      </Switch>
+    </Router>
   );
 }
 
