4ed7fe2d1d783acacd5c6ffd4f52501cd747e92a
Jasenko Bozinovic <jasenkobozinovic@gmail.com>
2024-06-01 06:39:53 +0200

    chore: Update npm dependencies and add sass and universal-cookie packages

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import ProtectedAuth from './auth/ProtectedAuth';
+
+
 import HomePage from './pages/HomePage.js';
 import AdminDashboard from './pages/AdminDashboard';
 import UserDashboard from './pages/UserDashboard';
-import NotFoundPage from './pages/NotFoundPage';
+import Error404 from './pages/errors/Error401.js';
+import Error401 from './pages/errors/Error404.js';
+import Error403 from './pages/errors/Error403.js';
+
+
+import './App.css';
 
 function App() {
   return (
-     <BrowserRouter>
+    <BrowserRouter>
       <Routes>
-        <Route  path="/" element={<HomePage />} />
-        <Route path="/user-dashboard" component={UserDashboard} />
-        <Route path="/admin-dashboard" component={AdminDashboard} />
-        <Route path="/404" component={NotFoundPage} />
-        <Route path='*' component={NotFoundPage} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/user-dashboard" element={
+          <ProtectedAuth
+            roles={['user']}
+          >
+            <UserDashboard />
+          </ProtectedAuth>
+        } />
+        <Route path="/admin-dashboard" element={
+          <ProtectedAuth
+            roles={['admin']}
+          >
+            <AdminDashboard />
+          </ProtectedAuth>
+        } />
+        <Route path="/404" element={<Error404 />} />
+        <Route path="/401" element={<Error401 />} />
+        <Route path="/403" element={<Error403 />} />
+        <Route path='*' element={<Error404 />} />
       </Routes>
-     </BrowserRouter>
+    </BrowserRouter >
   );
 }
 
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  devtool: 'source-map',
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      }
+    ]
+  }
+};
