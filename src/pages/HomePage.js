ead3618634d1a26dac7296ecccabc15b632a00f3
Jasenko Bozinovic <jasenkobozinovic@gmail.com>
2024-06-05 02:19:21 +0200

    chore: Remove unused mixins and update styles

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,30 +1,18 @@
-import React from 'react';
-import GameCard from '../components/GameCard/GameCard.js';
-import fakeData from './../utils/fakeData.json';
+import React from "react";
+import GameCard from "../components/GameCard/GameCard.js";
+import fakeData from "./../utils/fakeData.json";
 
-
-
-
-import '../styles/Homepage.css';
-
-
+import "../styles/Homepage.css";
 
 function HomePage() {
-
   const gameList = fakeData.items.map((item) => {
-    return (
-      <GameCard key={item.id} item={item} />
-    );
+    return <GameCard key={item.id} item={item} />;
   });
   return (
-
     <div className="home-page container">
       <h1 className="page-title">Welcome to our store</h1>
-      <div className="row">
-        {gameList}
-      </div>
+      <div className="row">{gameList}</div>
     </div>
-
   );
 }
 
